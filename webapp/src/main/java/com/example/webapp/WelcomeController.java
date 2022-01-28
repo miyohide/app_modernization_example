ac67c4fa3bd6f8d7b44988ea3202e957f2e899cf
Hidenori Miyoshi <miyohide@gmail.com>
2022-01-28 17:55:48 +0900

    create index page

---
+++ webapp/src/main/java/com/example/webapp/WelcomeController.java
@@ -0,0 +1,14 @@
+package com.example.webapp;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class WelcomeController {
+    @GetMapping("/")
+    public String index(Model model) {
+        model.addAttribute("message", "Hello web app");
+        return "index";
+    }
+}
