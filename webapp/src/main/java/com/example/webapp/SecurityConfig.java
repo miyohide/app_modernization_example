13adf688b685134c71e3506f6a81a7a792f6f45f
Hidenori Miyoshi <miyohide@gmail.com>
2022-02-02 20:23:09 +0900

    use original login page

--- webapp/src/main/java/com/example/webapp/SecurityConfig.java
+++ webapp/src/main/java/com/example/webapp/SecurityConfig.java
@@ -9,11 +9,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
+                .antMatchers("/webjars/**").permitAll()
                 .mvcMatchers("/").permitAll()
                 .anyRequest()
                 .authenticated()
                 .and()
                 .formLogin()
+                .loginPage("/login").permitAll()
                 .defaultSuccessUrl("/");
     }
 }
