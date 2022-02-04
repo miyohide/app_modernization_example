70ff45ca159a355ba3cf0da68394481cf2022b77
Hidenori Miyoshi <miyohide@gmail.com>
2022-02-04 18:30:20 +0900

    use inMemory auth

--- webapp/src/main/java/com/example/webapp/SecurityConfig.java
+++ webapp/src/main/java/com/example/webapp/SecurityConfig.java
@@ -1,11 +1,29 @@
 package com.example.webapp;
 
-import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
-@Configuration
+@EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    // パスワードのハッシュ化
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+            .withUser("hoge")
+            .password(passwordEncoder().encode("123456"))
+            .roles("USER");
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
