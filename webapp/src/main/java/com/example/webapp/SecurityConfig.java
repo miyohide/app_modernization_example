2a7921ada7e5d0f556cbe0508a69c304350ce415
Hidenori Miyoshi <miyohide@gmail.com>
2022-02-05 16:51:58 +0900

    implement db auth

--- webapp/src/main/java/com/example/webapp/SecurityConfig.java
+++ webapp/src/main/java/com/example/webapp/SecurityConfig.java
@@ -1,16 +1,21 @@
 package com.example.webapp;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Autowired
+    private UserDetailsService userDetailsService;
+
     // パスワードのハッシュ化
     @Bean
     public PasswordEncoder passwordEncoder() {
@@ -24,10 +29,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-            .withUser("hoge")
-            .password(passwordEncoder().encode("123456"))
-            .roles("USER");
+        auth.userDetailsService(userDetailsService);
     }
 
     @Override
