5b86bfc1beaa4b1f5820eddf7d918c2d85d1e39b
Hidenori Miyoshi <miyohide@gmail.com>
2022-02-10 20:35:34 +0900

    create first test

---
+++ db/src/test/java/org/example/MainTest.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class MainTest {
+  @Test
+  void myFirstTest() {
+    assertEquals(2, 1 + 1);
+  }
+}
