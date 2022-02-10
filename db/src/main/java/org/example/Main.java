4d8f386200bcc92b52eb82d286cee33f757e75e0
Hidenori Miyoshi <miyohide@gmail.com>
2022-02-10 21:02:15 +0900

    add test for get config file

--- db/src/main/java/org/example/Main.java
+++ db/src/main/java/org/example/Main.java
@@ -1,10 +1,16 @@
 package org.example;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import com.typesafe.config.Config;
 import com.typesafe.config.ConfigFactory;
 import org.flywaydb.core.Flyway;
 
 public class Main {
+    private Config config;
+    private Map<String, String> appConfig;
+
     public static void main(String[] args) {
         Config config = ConfigFactory.load();
         String url = config.getString("datasource.url");
@@ -14,4 +20,17 @@ public class Main {
                         .dataSource(url, user, password).load();
         flyway.migrate();
     }
+
+    public Main() {
+        this.config = ConfigFactory.load();
+        this.appConfig = new HashMap<>();
+    }
+
+    public Map<String, String> getConfig() {
+        this.appConfig.put("url", this.config.getString("datasource.url"));
+        this.appConfig.put("user", this.config.getString("datasource.user"));
+        this.appConfig.put("password", this.config.getString("datasource.password"));
+
+        return this.appConfig;
+    }    
 }
--- db/src/test/java/org/example/MainTest.java
+++ db/src/test/java/org/example/MainTest.java
@@ -2,11 +2,17 @@ package org.example;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.util.Map;
+
 import org.junit.jupiter.api.Test;
 
 public class MainTest {
   @Test
-  void myFirstTest() {
-    assertEquals(2, 1 + 1);
+  void noEnvironmentVariable() {
+    Main m = new Main();
+    Map<String, String> appConfig = m.getConfig();
+    assertEquals(appConfig.get("url"), "jdbc:postgresql://localhost:5432/azureuser");
+    assertEquals(appConfig.get("user"), "azureuser");
+    assertEquals(appConfig.get("password"), "superverystrongpassword");
   }
 }
