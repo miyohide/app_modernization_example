483c980373adbc5447b9dbece55d273926088261
Hidenori Miyoshi <miyohide@gmail.com>
2022-02-18 21:05:29 +0900

    change users table and add authorities table

--- db/src/main/resources/db/migration/V3.0.0__create_user_table.sql
+++ db/src/main/resources/db/migration/V3.0.0__create_user_table.sql
@@ -2,8 +2,14 @@ create table users
 (
   id bigserial constraint users_pk primary key,
   name VARCHAR(100) NOT NULL UNIQUE,
-  password VARCHAR(100) NOT NULL,
+  password VARCHAR(100) NOT NULL
+);
+
+create table authorities (
+  id bigserial constraint authorities_pk primary key,
+  user_id bigint references users(id),
   authority VARCHAR(100)
 );
 
 create unique index users_id_uindex on users (id);
+create unique index authorities_id_uindex on authorities (id);
--- db/src/main/resources/db/migration/V4.0.0__insert_users.sql
+++ db/src/main/resources/db/migration/V4.0.0__insert_users.sql
@@ -1,4 +1,6 @@
 -- 名前：hoge、パスワード：123456
-insert into users(name, password, authority) values('hoge', '$2a$10$KBVjUqJO8wdYklH9dV4RFOOMzd0rJhjEJtwJDeEep3GTAefMbCynO', 'ROLE_USER');
+insert into users(name, password) values('hoge', '$2a$10$KBVjUqJO8wdYklH9dV4RFOOMzd0rJhjEJtwJDeEep3GTAefMbCynO');
+insert into authorities (user_id, authority) values ( ( select id from users where name = 'hoge'), 'ROLE_USER');
 -- 名前：hoge2、パスワード：hogehoge
-insert into users(name, password, authority) values('hoge2', '$2a$10$zG75DibVEeDlOze0T/2G6Ok6e0yNBIx4aUW4M.s4dmpYwnQ4x2P0m', 'ROLE_USER');
+insert into users(name, password) values('hoge2', '$2a$10$zG75DibVEeDlOze0T/2G6Ok6e0yNBIx4aUW4M.s4dmpYwnQ4x2P0m');
+insert into authorities (user_id, authority) values ( ( select id from users where name = 'hoge2'), 'ROLE_USER');
