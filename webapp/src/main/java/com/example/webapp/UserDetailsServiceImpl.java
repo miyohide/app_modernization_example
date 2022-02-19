4480988df505b0da10d646ce13fb1aeba9d9d8e8
Hidenori Miyoshi <miyohide@gmail.com>
2022-02-19 20:30:16 +0900

    change table structure

--- webapp/src/main/java/com/example/webapp/UserDetailsServiceImpl.java
+++ webapp/src/main/java/com/example/webapp/UserDetailsServiceImpl.java
@@ -2,6 +2,7 @@ package com.example.webapp;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,11 +22,15 @@ public class UserDetailsServiceImpl implements UserDetailsService {
   @Override
   public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
     try {
-      String sql = "SELECT * FROM users WHERE name = ?";
+      String sql = "SELECT id, password FROM users WHERE name = ?";
       Map<String, Object> map = jdbcTemplate.queryForMap(sql, username);
       String password = (String)map.get("password");
       Collection<GrantedAuthority> authorities = new ArrayList<>();
-      authorities.add(new SimpleGrantedAuthority((String)map.get("authority")));
+      String authority_sql = "SELECT authority FROM authorities WHERE user_id = ?";
+      List<Map<String, Object>> authorities_result = jdbcTemplate.queryForList(authority_sql, (Long)map.get("id"));
+      for (Map<String,Object> map2 : authorities_result) {
+        authorities.add(new SimpleGrantedAuthority((String)map2.get("authority")));
+      }
       return new UserDetailsImpl(username, password, authorities);
     } catch (Exception e) {
       throw new UsernameNotFoundException("User not found.", e);
